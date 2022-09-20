04c8d208e89118338e78f324c2f8046c96c9d9b4
Lucas <Lucas@DESKTOP-NVHM955>
2022-09-20 16:52:49 -0300

    commit1

---
+++ src/controller/OperacaoController.java
@@ -0,0 +1,34 @@
+package controller;
+
+
+import br.com.leandrocolevati.pilhaint.Pilha;
+
+public class OperacaoController {
+
+	public OperacaoController() {
+
+	}
+
+	public Pilha maiorValor (Pilha pilha) throws Exception {
+		Pilha pilha2 = pilha; //duplica pilha
+		int tamanho = pilha.size();
+		
+		while (tamanho > 1) {
+			int aux1 = pilha.pop();
+			int aux2 = pilha.pop();
+			
+			if (aux1 > aux2) {
+				pilha.push(aux1);
+			}
+			else {
+				pilha.push(aux2);
+			}
+			tamanho = pilha.size();
+		}
+		
+		System.out.println("========================");
+		System.out.println(pilha);
+		
+		return pilha;
+	}
+}
